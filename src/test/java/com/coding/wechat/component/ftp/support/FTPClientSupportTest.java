1c59b02b9b3a45a05344d20df15d9f7a0087fc1e
Rushing <mingli@ishanshan.com>
2018-06-21 20:25:56 +0800

    update:20180621-04

---
+++ src/test/java/com/coding/wechat/component/ftp/support/FTPClientSupportTest.java
@@ -0,0 +1,21 @@
+package com.coding.wechat.component.ftp.support;
+
+import com.coding.wechat.component.ftp.FTPConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FTPClientSupportTest {
+
+    @Autowired FTPConfig ftpConfig;
+
+    @Test
+    public void connect() {
+        FTPClientSupport ftpClient = FTPClientSupport.getInstance(ftpConfig.getServer("default"));
+        ftpClient.connect();
+    }
+}
