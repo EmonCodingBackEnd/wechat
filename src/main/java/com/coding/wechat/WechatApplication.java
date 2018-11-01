f6e7932d2a41384b188f5fa0d7a5425a78353df9
Rushing <liming20110711@163.com>
2018-11-02 07:30:18 +0800

    update:20181102-01:引入SpringSession

--- src/main/java/com/coding/wechat/WechatApplication.java
+++ src/main/java/com/coding/wechat/WechatApplication.java
@@ -2,10 +2,13 @@ package com.coding.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-// @EnableCaching
+@EnableCaching
+@EnableRedisHttpSession
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
