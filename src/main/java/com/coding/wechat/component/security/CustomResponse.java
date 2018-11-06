f4f78cff24ac3ca6e058d537ee1af5c554f7e5b1
Rushing <mingli@ishanshan.com>
2018-11-06 15:43:03 +0800

    update:20181106-05

--- src/main/java/com/coding/wechat/component/security/CustomResponse.java
+++ src/main/java/com/coding/wechat/component/security/CustomResponse.java
@@ -1,8 +1,19 @@
+/*
+ * 文件名称：CustomResponse.java
+ * 系统名称：[系统名称]
+ * 模块名称：[模块名称]
+ * 软件版权：Copyright (c) 2011-2018, liming20110711@163.com All Rights Reserved.
+ * 功能说明：[请在此处输入功能说明]
+ * 开发人员：Rushing0711
+ * 创建日期：20181106 15:42
+ * 修改记录：
+ * <Version>        <DateSerial>        <Author>        <Description>
+ * 1.0.0            20181106-01         Rushing0711     M201811061542 新建文件
+ ********************************************************************************/
 package com.coding.wechat.component.security;
 
 import java.io.Serializable;
 
-/** Created by xiaogang on 2016/12/25. */
 public class CustomResponse<T extends Serializable> implements Serializable {
 
     private static final long serialVersionUID = -4605251603996640240L;
